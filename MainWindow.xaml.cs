b7ecc26991782c0b84b2f8f1ebe402e621201a2f
Tvuce <wowmrx91@hotmail.it>
2023-10-08 21:55:20 +0200

    up

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testbinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public class User
+    {
+        public string name {  get; set; }
+        public string surn { get; set; }
+        public int age { get; set; }
+
+        
+    }
+    
+    
+
+
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<User> o = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+            o = new ObservableCollection<User>();
+            testgrid.DataContext = o;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            User p = new User() { name = "cane", surn = "lupo", age = 15 };
+            o.Add(p);
+        }
+    }
+
+}
+
